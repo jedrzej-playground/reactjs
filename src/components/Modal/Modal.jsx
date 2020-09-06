8a083020dd923079e8149bca8dfc16e06b418613
jedrzej_danowski <jedrzej_danowski@epam.com>
2020-09-06 17:31:21 +0200

    Added portal to sent modal into magical journey + some css adjustements

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
+import {createPortal} from 'react-dom';
 import './Modal.css';
 
-const Modal = ({onClose, title, content, buttons}) => 
-    <div className="modal">
-        <button className="modal-close-button" onClick={onClose}>X</button>
-        <div className="modal-title">
-            {title}
-        </div>
+const modalRoot = document.querySelector('#modal-root');
 
-        <div className="modal-content">
-            {content}
-        </div>
+const Modal = ({onClose, title, content, buttons}) => {
 
-        <div className="modal-footer">
-            {buttons}
-        </div>
-    </div>
+    return createPortal(
+        <div className="modal-blur-wrapper">
+            <div className="modal">
+                <button className="modal-close-button" onClick={onClose}>X</button>
+                <div className="modal-title">
+                    {title}
+                </div>
+
+                <div className="modal-content">
+                    {content}
+                </div>
+
+                <div className="modal-footer">
+                    {buttons}
+                </div>
+            </div>
+        </div>,
+    modalRoot
+    );
+}
 
 export default Modal;
